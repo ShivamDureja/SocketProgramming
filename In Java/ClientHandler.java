c01e155c2ef5c9cc972fb659ac29eccff975590a
ShivamDureja <shivamdureja13@gmail.com>
2024-01-05 01:49:20 +0530

    modified

--- In Java/ClientHandler.java
+++ In Java/ClientHandler.java
@@ -8,18 +8,18 @@ public class ClientHandler {
         this.socket = socket;
     }
 
-    public void run() throws IOException {
-        String text = "Dummy text from Client";
+    public void run(int c) throws IOException {
+        String text = "Dummy text from Client Number " + c;
         int count = 0;
         do {
             count++;
-            System.out.println("Recieved message from Client " + text);
+            System.out.println("Recieved message from Client Number " + c + " : " + text);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 System.out.println("Thread Connection Interrupted " + ex.getMessage());
             }
-            System.out.println("Thanks");
+            System.out.println("Thanks " + c);
         } while (count < 5);
 
         this.socket.close();
--- In Java/Server.java
+++ In Java/Server.java
@@ -4,16 +4,32 @@ import java.net.*;
 public class Server {
     public static void main(String[] args) throws IOException {
         int port = 8080;
-        try (ServerSocket serverSocket = new ServerSocket()) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server is listening to port " + port);
-
             while (true) {
-                Socket clienSocket = serverSocket.accept();
+                Socket socket = serverSocket.accept();
                 System.out.println("New Client Connected!!");
 
-                new ClientHandler(clienSocket).run();
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+                    String clientMessage = in.readLine();
+                    System.out.println("Message recieved from client :  " + clientMessage);
+                    out.println("Echo from server: " + clientMessage);
+
+                }
             }
-        } catch (IOException ex) {
+        }
+        // try (ServerSocket serverSocket = new ServerSocket(port)) {
+        // System.out.println("Server is listening to port " + port);
+        // int count = 0;
+        // while (true) {
+        // Socket clienSocket = serverSocket.accept();
+        // System.out.println("New Client Connected!!, count " + count);
+
+        // new ClientHandler(clienSocket).run(count);
+        // }
+        catch (IOException ex) {
             System.out.println("Server Exception " + ex.getMessage());
             ex.getStackTrace();
         }
