fb9481c395724e68a87ce2e0cb9b8c006aa1dd00
ShivamDureja <shivamdureja13@gmail.com>
2024-01-07 12:50:21 +0530

    comments added

--- In Java/Client.java
+++ In Java/Client.java
@@ -7,16 +7,25 @@ import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+        // need to provide hostname with port in client side
         String hostName = "localhost";
         int port = 8080;
-
+        // creates new client socket object
         try (Socket socket = new Socket(hostName, port)) {
+            // socket.getOutputStream() : recieves output stream from server to client
+            // PrintWriter ensures byte stream gets converted to character stream
+            // lushing ensures that the data is immediately sent over the network, making it
+            // useful for real-time communication.
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             out.println("Hello Server, I am your client !!");
-
+            // socket.getInputStream() : recieves the bytes coming from the client
+            // new InputStreamReader(socket.getInputStream()) : to convert bytes stream to
+            // characters stream
+            // Buffered Reader for high level abstraction to get text from character based
+            // stream
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String response = in.readLine();
-            System.out.println("Server response: " + response);  
+            System.out.println("Server says: " + response);
 
         } catch (UnknownHostException ex) {
             System.out.println("Server UnknownHostException " + ex.getMessage());
--- In Java/Server.java
+++ In Java/Server.java
@@ -3,33 +3,33 @@ import java.net.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
+        // Port Number for server to listen on
         int port = 8080;
+        // creates a new server to listen incoming requests
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server is listening to port " + port);
+            // To keep server running we used infinite while loop
             while (true) {
+                // accept a new client connection and create a socket object
                 Socket socket = serverSocket.accept();
+                // To confirm if client is connected
                 System.out.println("New Client Connected!!");
-
+                // socket.getInputStream() : recieves the bytes coming from the client
+                // new InputStreamReader(socket.getInputStream()) : to convert bytes stream to
+                // characters stream
+                // Buffered Reader for high level abstraction to get text from character based
+                // stream
                 try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                     PrintWriter out = new PrintWriter(socket.getOutputStream());
-
+                    // get the text message from client
                     String clientMessage = in.readLine();
                     System.out.println("Message recieved from client :  " + clientMessage);
+                    // sends a message back to the client
                     out.println("Echo from server: " + clientMessage);
 
                 }
             }
-        }
-        // try (ServerSocket serverSocket = new ServerSocket(port)) {
-        // System.out.println("Server is listening to port " + port);
-        // int count = 0;
-        // while (true) {
-        // Socket clienSocket = serverSocket.accept();
-        // System.out.println("New Client Connected!!, count " + count);
-
-        // new ClientHandler(clienSocket).run(count);
-        // }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             System.out.println("Server Exception " + ex.getMessage());
             ex.getStackTrace();
         }
